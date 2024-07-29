55e631c79b4f1204b202dbff0289c352763ba60d
devcom <kwp@tlen.pl>
2024-07-29 11:45:26 +0200

    test1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String name;
+        int liczba_1;
+        int liczba_2;
+        System.out.println("Hello and welcome!");
+        System.out.println("tomek");
+        name = "Tomek";
+        liczba_1 = 4;
+        liczba_2 = 9;
+
+        System.out.println(name);
+        System.out.println("Suma liczb a i b wynosi:" + liczba_1 + liczba_2);
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
